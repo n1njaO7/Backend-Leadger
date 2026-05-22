51bfa0cf0359f3dcbfaacd53e6d4e5c93088ed54
n1njaO7 <n1njasri07@gmail.com>
2026-05-22 23:24:48 +0530

    used cookies to save token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -24,6 +24,15 @@ async function userRegisterController(req,res) {
 
     const token =jwt.sign({userId:user._id},process.env.JWT_SECRET,{expiresIn:"3d"})
     
+    res.cookies("token",token)
+    res.status(201).json({
+        user:{
+            _id:user._id,
+            email:user.email,
+            name:user.name
+        },
+        token
+    })
 }
 
 module.exports={
