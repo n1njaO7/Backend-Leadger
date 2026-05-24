9c24299b464cc288300581a2ed42b41e7d109ab7
n1njaO7 <n1njasri07@gmail.com>
2026-05-24 19:43:27 +0530

    created transaction routes

--- src/app.js
+++ src/app.js
@@ -5,11 +5,15 @@ const app = express()
 
 const authRouter = require("./routes/auth.routes")
 const accountRouter = require("./routes/account.routes")
+const transactionRoutes = require("./routes/transaction.routes")
 
 app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/auth",authRouter)
+app.use("api/accounts",accountRouter)
+app.use("api/transactions",transactionRoutes)
+
 
 app.use("")
 
