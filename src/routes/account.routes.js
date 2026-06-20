62f1abbaa1052812f68d400b8bb015d003e1d84b
n1njaO7 <n1njasri07@gmail.com>
2026-06-21 04:12:44 +0530

    changed name

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const authMiddleware = require("../middleware/auth.middleware")
+const authMiddleware = require("../middlewares/auth.middleware")
 const accountController = require("../controllers/account.controller")
 
 
--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const authMiddleware = require('../middleware/auth.middleware');
+const authMiddleware = require('../middlewares/auth.middleware');
 const transactionController = require("../controllers/transaction.controller")
 
 const transactionRoutes = Router();
